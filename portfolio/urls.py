cf3f761727b258061ad64d05a4a0f94d34b81805
Jesse Lee Ross <jesseross001@gmail.com>
2024-01-27 13:55:15 +0000

    Correct urls for each app and view

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('portfolios/', views.portfolios, name='portfolios'),
+    path('', views.portfolios, name='portfolios'),
 ]
