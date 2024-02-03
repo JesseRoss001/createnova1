755d5accada77aaf2065766a9558c22caff441df
Jesse Lee Ross <jesseross001@gmail.com>
2024-02-03 12:04:44 +0000

    Going to get icons8 images

--- testimonials/views.py
+++ testimonials/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def testimonials(request):
-    return render(request, 'testimonials/testimonials.html')
+        # Create a range of numbers for the Lottie files
+    lottie_numbers = range(1, 24)  # This will create a range from 1 to 23
+    context = {'lottie_numbers': lottie_numbers}
+    return render(request, 'testimonials/testimonials.html',context)
