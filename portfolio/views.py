35e0a7c91129c30a311a36025bb34cd2d513223c
Jesse Lee Ross <jesseross001@gmail.com>
2024-02-05 13:48:37 +0000

    Add update for portfolio

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.decorators import login_required
 from portfolio.models import ContentCreator
 from portfolio.forms import ContentCreatorProfileForm
 from django.contrib import messages
-
+import json
+MAX_UPLOAD_SIZE = 300 * 1024  # 300 KB
 @login_required
 def your_profile_view(request):
     content_creator = request.user.contentcreator
@@ -11,15 +12,29 @@ def your_profile_view(request):
         messages.error(request, 'Your account is pending approval.')
         return redirect('home')
 
+    initial_data = {}
+    if content_creator.social_media_links:
+        # Convert the JSON string back to a Python dictionary
+        initial_data['social_media_links'] = json.loads(content_creator.social_media_links)
+    else:
+        initial_data['social_media_links'] = {}
+
     if request.method == 'POST':
         form = ContentCreatorProfileForm(request.POST, request.FILES, instance=content_creator)
+
+        # Add logic to pack social media links into JSON before saving
+        social_media_links = {}
+        for key in request.POST:
+            if key.startswith('social_media_'):
+                social_media_links[key] = request.POST[key]
+        content_creator.social_media_links = json.dumps(social_media_links)
+
         if form.is_valid():
             form.save()
             messages.success(request, 'Your profile has been updated successfully.')
             return redirect('yourprofile')
     else:
-        # Prepopulate the form with existing data
-        form = ContentCreatorProfileForm(instance=content_creator)
+        form = ContentCreatorProfileForm(initial=initial_data, instance=content_creator)
 
     context = {
         'form': form,
